b84964a61938f1e1e10a4d217f06178d04da0b56
eduardoreicherts <dudureichert86@gmail.com>
2023-08-01 19:50:09 -0300

    Running

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import app from './app';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log('listenning at port ' + port));
